b9f84308937aaf92b6f8baf85e90164f9926eca0
jonas <pedrojonas.pj77@gmail.com>
2022-10-13 08:08:28 -0300

    Prepare for deploy

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import connection from './Database/connection.js';
+
+dotenv.config();
+const server = express();
+
+server.use(json());
+server.use(cors());
+
+
+server.get("/users" , async (req, res) => {
+    try{
+        const users = await connection.query('SELECT * FROM users;');
+        res.send(users.rows).status(200);
+    } catch {
+        res.sendStatus(422);
+    }
+});
+
+
+server.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
